469b8aec8f17bd3af428fa0b577c04d50dffd671
kimhyeyun <cqqwer@gmail.com>
2020-10-05 18:26:30 +0900

    컴퓨터 보안 2차 과제

---
+++ 컴퓨터보안/2차 과제/miller_rabin.py
@@ -0,0 +1,67 @@
+"""
+miller-rabin prime test
+
+Test if n is prime with error probability less than 2^(−𝑠).
+
+"""
+
+import random
+from exponentiation import exp
+
+Prime = 0
+Composite = 1
+
+
+def miller_rabin(n, s):
+    if n == 2:
+        return Prime
+    elif n % 2 == 0:
+        return Composite
+
+    for _ in range(s):
+        a = random.randint(1, n-1)
+        if test(a, n) == True:
+            return Composite
+
+    return Prime
+
+def test(a, n):
+    y = n-1
+    cnt = 0
+    while(1):
+        if(y%2==0):
+            y //= 2
+            cnt+=1
+        else:
+            break
+    t = cnt
+    u = y
+
+    x_0 = exp(a,u,n)
+
+    for i in range(t):
+        x = exp(x_0,2,n)
+        if x==1 and x_0!=1 and x_0!=n-1:
+            return True
+        x_0 = x
+        
+
+    if x!=1:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+
+    primes = [7879, 7883, 7901, 7907, 7919, 7927, 7933, 7937, 7949, 7951,
+              7963, 7993, 8009, 8011, 8017, 8039, 8053, 8059, 8069, 8081,
+              8087, 8089, 8093, 8101, 8111, 8117, 8123, 8147, 8161, 8167,8742,8743]
+
+    for p in primes:
+        result = miller_rabin(p, 20)
+        if result == Prime:
+            print("Prime")
+        elif result == Composite:
+            print("Composite")
+        else:
+            print("Undefined")
